330f65641c0022ed51c4dded1f71282fd0f14e32
Yubing Hou <houyubing24@gmail.com>
2022-05-27 19:01:17 -0500

    can output string from express

---
+++ packages/server/src/index.ts
@@ -0,0 +1,11 @@
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const port: number = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript server');
+});
+app.listen(port, () => {
+    console.log(`[server]: Server is running at https://localhost:${port}`);	
+});
