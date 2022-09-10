8a767adfe023efac64258e14fb83b0173e80f806
Yubing Hou <houyubing24@gmail.com>
2022-09-10 16:04:19 -0500

    updated CI; updated testing scripts

--- projects/frontend/karma.conf.js
+++ projects/frontend/karma.conf.js
@@ -38,6 +38,14 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      Headless: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox'
+        ]
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
