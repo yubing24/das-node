10c87eddbd0bb77529bb130c4bc6c6c2b8e438b4
Yubing Hou <houyubing24@gmail.com>
2022-09-10 23:18:14 -0500

    can correctly serve angular front-end

--- projects/server/src/index.ts
+++ projects/server/src/index.ts
@@ -1,15 +1,27 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import serveStatic from "serve-static";
+import path from "path";
 
 const app = express();
+
+// TODO: make this an env variable
 const port = 3000;
+
+// TODO: make this an env variable
+const SPA_DIRECTORY = "../../frontend/dist/das-frontend";
 
 dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("Express + TypeScript server");
+app.get("/health", (req, res) => {
+  res.send("Good!");
 });
+
+app.use(serveStatic(path.join(__dirname, SPA_DIRECTORY)));
+
 app.listen(port, () => {
+  const spaPath = path.join(__dirname, SPA_DIRECTORY, "index.html");
+  console.log(`Assumed SPA PATH: ${spaPath}`);
   console.log(`[server]: Server is running at https://localhost:${port}`);
 });
 
